40f3b501c2a25aa2aab011f49d4a301489ad3db3
GermanHv <ghove7@gmail.com>
2022-05-05 16:58:35 -0500

    7. Agregando ESLint y corrigiendo archivos con Linter

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,18 +1,18 @@
-const ExplorerService = require("./../../lib/services/ExplorerService")
+const ExplorerService = require("./../../lib/services/ExplorerService");
 
 describe ("Test para ExplorerSevice", ()=>{
     test("Requerimiento 1: Calcular todos los explorers en una mision", ()=> {
         const explorers = [{ mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-        expect (explorersInNode.length).toBe(1)
-    })
+        expect (explorersInNode.length).toBe(1);
+    });
     test("Requerimiento 2: Calcular explorers en una misión", () => {
         const explorer = [{ githubUsername: "user1", mission: "node" }];
         const explorersInNode = ExplorerService.getExplorersUsernamesByMission(
-          explorer,
-          "node"
+            explorer,
+            "node"
         );
         expect(explorersInNode[0]).toBe("user1");
-      });
+    });
 
-})
+});
